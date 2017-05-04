9887b2e28e9635c9922b493c5ba54853c0661d05
zhangslim <lidiazhi@qq.com>
2017-05-04 18:40:41 +0800

    Add files via upload

---
+++ ShoppingMall/src/cn/edu/zhku/jsj/Model/address.java
@@ -0,0 +1,40 @@
+package cn.edu.zhku.jsj.Model;
+
+public class address {
+private int id;
+private int user_id;
+private int receivertel;
+private String receiver;
+private String location;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getUser_id() {
+	return user_id;
+}
+public void setUser_id(int user_id) {
+	this.user_id = user_id;
+}
+public int getReceivertel() {
+	return receivertel;
+}
+public void setReceivertel(int receivertel) {
+	this.receivertel = receivertel;
+}
+public int getReceiver() {
+	return receiver;
+}
+public void setReceiver(String receiver) {
+	this.receiver = receiver;
+}
+public int getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+
+}
