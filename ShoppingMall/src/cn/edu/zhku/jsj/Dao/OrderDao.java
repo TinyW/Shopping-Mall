c0292fcfe6bcd3e8defcfb04a39c3192cd4075d6
zhangslim <lidiazhi@qq.com>
2017-06-01 10:56:54 +0800

    Add files via upload

--- ShoppingMall/src/cn/edu/zhku/jsj/Dao/OrderDao.java
+++ ShoppingMall/src/cn/edu/zhku/jsj/Dao/OrderDao.java
@@ -1,22 +1,28 @@
 package cn.edu.zhku.jsj.Dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import cn.edu.zhku.jsj.Model.Address;
+import cn.edu.zhku.jsj.Model.Good;
 import cn.edu.zhku.jsj.Model.Order;
 import cn.edu.zhku.jsj.Model.OrderItem;
 import cn.edu.zhku.jsj.Model.Pager;
 import cn.edu.zhku.jsj.Model.User;
 import cn.edu.zhku.jsj.Util.BaseUtil;
+import cn.edu.zhku.jsj.Util.DBUtil;
 
 public class OrderDao {
 	public boolean save(Order order)
 	{
 		boolean flag=false;
-		String sql="insert into order(createdate,state,user_id,address_id)value(?,?,?,?)";
-		Object []params={order.getCreateDate(),order.getState(),order.getUser().getId(),order.getAddress().getId()};
+		String sql="insert into `order`(createdate,ostate,user_id,address_id)value(?,?,?,?)";
+		Object []params={order.getCreateDate(),order.getOstate(),order.getUser().getId(),order.getAddress().getId()};
 		BaseUtil<Order> util=new BaseUtil<Order>();
 		flag=util.update(sql, params);
 		return flag;
@@ -24,8 +30,8 @@ public class OrderDao {
 	public boolean update(Order order)
 	{
 		boolean flag=false;
-		String sql="update order set state=?,createdate=?,address_id=?,user_id=? where id=?";
-		Object []params={order.getState(),order.getCreateDate(),order.getAddress().getId(),order.getUser().getId(),order.getId()};
+		String sql="update `order` set ostate=?,createdate=?,address_id=?,user_id=? where id=?";
+		Object []params={order.getOstate(),order.getCreateDate(),order.getAddress().getId(),order.getUser().getId(),order.getId()};
 		BaseUtil<Order> util=new BaseUtil<Order>();
 		flag=util.update(sql, params);
 		return flag;
@@ -33,7 +39,7 @@ public class OrderDao {
 	public boolean delete(int id)
 	{
 		boolean flag=false;
-		String sql="delete from order where id=?";
+		String sql="delete from `order` where id=?";
 		Object[]params={id};
 		BaseUtil<Order> util=new BaseUtil<Order>();
 		flag=util.update(sql, params);
@@ -42,7 +48,7 @@ public class OrderDao {
 	public Order load(Map<String,Object> map,boolean flag1,boolean flag2,boolean flag3)
 	{
 		Order order=new Order();
-		String sql="select * from order where 1=1";
+		String sql="select * from `order` where 1=1";
 		for(String key:map.keySet())
 			sql=sql+" and "+key+"=?";
 		Object []params=map.values().toArray();
@@ -50,7 +56,7 @@ public class OrderDao {
 		order=(Order) util.QueryOne(Order.class, sql, params);
 		if(flag1)
 		{
-			String sql1="select * from address where id=(select id from order where id=?)";
+			String sql1="select * from address where id=(select id from `order` where id=?)";
 			BaseUtil<Address> addressUtil=new BaseUtil<Address>();
 			Object[]p={order.getId()};
 			order.setAddress((Address)addressUtil.QueryOne(Address.class, sql1, p));
@@ -58,7 +64,7 @@ public class OrderDao {
 		}
 		if(flag2)
 		{
-			String sql1="select * from user where id=(select id from order where id=?)";
+			String sql1="select * from user where id=(select id from `order` where id=?)";
 			BaseUtil<User> addressUtil=new BaseUtil<User>();
 			Object[]p={order.getId()};
 			order.setUser((User)addressUtil.QueryOne(User.class, sql1, p));
@@ -75,13 +81,15 @@ public class OrderDao {
 	public List<Order> list(Map<String,Object>map,Pager pager,String order,String choose)
 	{
 		List<Order>orders=new ArrayList<Order>();
-		String sql="select * from order where 1=1";
+		String sql="select * from `order` where 1=1";
 		for(String key:map.keySet())
 			sql=sql+" and "+key+"=?";
 		if("desc".equals(choose))
 			sql=sql+" order by ? desc limit ?,?";
 		else sql=sql+" order by ? asc limit ?,?";
+		
 		Object params=map.values().toArray();
+		System.out.println(params);
 		BaseUtil<Order> util=new BaseUtil<Order>();
 		orders=util.QueryList(Order.class, sql, params,order,pager.getCurrent(),pager.getEachRecord());
 		return orders;
@@ -89,7 +97,7 @@ public class OrderDao {
 	public int countOrder(Map<String,Object>map)
 	{
 		int result=0;
-		String sql="select count(*) from order where 1=1";
+		String sql="select count(*) from `order` where 1=1";
 		BaseUtil<Order> util=new BaseUtil<Order>();
 		if(null!=map)
 		{
@@ -101,5 +109,31 @@ public class OrderDao {
 		else
 			util.count(Order.class,sql);
 		return result;
+	}
+	public List<OrderItem> saveOrder(int item_id) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		conn = DBUtil.getConnection();
+		String sql = "select * from order_item where id=?";
+		List<OrderItem> items = new ArrayList<OrderItem>();
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, item_id);
+			rs = pstmt.executeQuery();
+			OrderItem orderitem = null;
+			Good good = null;
+			while(rs.next()){
+				orderitem = new OrderItem();
+				good = new OrderItemDao().getGoodById(rs.getInt("good_id"));
+				orderitem.setId(item_id);
+				orderitem.setNum(rs.getInt("num"));
+				orderitem.setGood(good);
+				items.add(orderitem);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return items;
 	}
 }
--- ShoppingMall/src/cn/edu/zhku/jsj/Dao/OrderItemDao.java
+++ ShoppingMall/src/cn/edu/zhku/jsj/Dao/OrderItemDao.java
@@ -1,9 +1,19 @@
 package cn.edu.zhku.jsj.Dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import cn.edu.zhku.jsj.Model.Good;
+import cn.edu.zhku.jsj.Model.Order;
 import cn.edu.zhku.jsj.Model.OrderItem;
+import cn.edu.zhku.jsj.Model.Pager;
 import cn.edu.zhku.jsj.Util.BaseUtil;
+import cn.edu.zhku.jsj.Util.DBUtil;
 
 public class OrderItemDao {
 	/*
@@ -62,5 +72,64 @@ public class OrderItemDao {
 		item=(OrderItem) util.QueryOne(OrderItem.class,sql,params);
 		return item;
 	}
+	public List<OrderItem> list(Map<String,Object>map,Pager pager,String order,String choose)
+	{
+		List<OrderItem> orderitem=new ArrayList<OrderItem>();
+		String sql="select * from order_item where 1=1";
+		for(String key:map.keySet())
+			sql=sql+" and "+key+"=?";
+		if("desc".equals(choose))
+			sql=sql+" order by ? desc limit ?,?";
+		else sql=sql+" order by ? asc limit ?,?";
+		Object params=map.values().toArray();
+		BaseUtil<OrderItem> util=new BaseUtil<OrderItem>();
+		orderitem=util.QueryList(OrderItem.class, sql, params,order,pager.getCurrent(),pager.getEachRecord());
+		return orderitem;
+	}
 	
+	//通过订单ID查询订单项
+	public List<OrderItem> getOrderItems(int order_id) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		conn = DBUtil.getConnection();
+		String sql = "select * from order_item where order_id=?";
+		
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, order_id);
+			rs = pstmt.executeQuery();
+			List<OrderItem> orderitems = new ArrayList<OrderItem>();
+			OrderItem orderitem = null;
+			Good good = null;
+			while(rs.next()){
+				orderitem = new OrderItem();
+				good = new Good();
+				good = getGoodById(rs.getInt("good_id"));//通过good_id取出good信息
+				orderitem.setId(rs.getInt("id"));
+				orderitem.setNum(rs.getInt("num"));
+				orderitem.setGood(good);
+				orderitems.add(orderitem);
+			}
+				return orderitems;
+	}
+	
+	//通过good_id查询good表中的信息
+	public Good getGoodById(int good_id) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		conn = DBUtil.getConnection();
+		String sql = "select * from good where id=?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, good_id);
+		rs = pstmt.executeQuery();
+		Good good = new Good();
+		while(rs.next()){
+			good.setGname(rs.getString("gname"));
+			good.setGprice(rs.getDouble("gprice"));
+			good.setId(good_id);
+			good.setIntroduction(rs.getString("introduction"));
+		}
+		return good;
+	}
 }
